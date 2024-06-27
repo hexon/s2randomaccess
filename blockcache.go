hexon/s2randomaccess

commit d90dbeaa9663f0597d3a49a7dbec03bb9b50febd

Bugfix: Preallocate the correct length for decompressing blocks
We need to allocate the amount of bytes for the decompressed payload,
not for the compressed payload.

Impact: We leaked the too-small-buffer and worse: injected a
non-power-of-2 back into the allocator

blockcache.go
@@ -43,7 +43,7 @@ func onEvicted(k lruKey, d *decompressedBlock) {
 }
 
 // getDecompressedBlock finds the S2 block
-func (s *Seeker) getDecompressedBlock(offset, length int64) ([]byte, func(), error) {
+func (s *Seeker) getDecompressedBlock(offset int64, length int) ([]byte, func(), error) {
 	em := easymutex.LockMutex(&s.mtx)
 	defer em.Unlock()
 	if db, ok := s.active[offset]; ok {
@@ -56,7 +56,7 @@ func (s *Seeker) getDecompressedBlock(offset, length int64) ([]byte, func(), err
 		return db.decompressed, db.deref, nil
 	}
 	em.Unlock()
-	buf := s.allocator.Alloc(int(length))
+	buf := s.allocator.Alloc(length)
 	decoded, err := s2.Decode(buf, s.data[offset:][:length])
 	if err != nil {
 		return nil, nil, err

randomaccess.go
@@ -115,7 +115,7 @@ func (s *Seeker) Get(offset, length int64) ([]byte, func(), error) {
 				skipUncompr -= int64(dLen)
 				break
 			}
-			block, deref, err := s.getDecompressedBlock(comprOff+headerSize+checksumSize, int64(len(chunk))-checksumSize)
+			block, deref, err := s.getDecompressedBlock(comprOff+headerSize+checksumSize, dLen)
 			if err != nil {
 				return nil, nil, err
 			}

commit 22b64280aea7a23e72524a992b6d788cec34b402

Stop mixing compressed and uncompressed lengths
Fixes a bug introduced in d90dbeaa9663f0597d3a49a7dbec03bb9b50febd

blockcache.go
@@ -43,7 +43,7 @@ func onEvicted(k lruKey, d *decompressedBlock) {
 }
 
 // getDecompressedBlock finds the S2 block
-func (s *Seeker) getDecompressedBlock(offset int64, length int) ([]byte, func(), error) {
+func (s *Seeker) getDecompressedBlock(offset int64, compressedLength, uncompressedLength int) ([]byte, func(), error) {
 	em := easymutex.LockMutex(&s.mtx)
 	defer em.Unlock()
 	if db, ok := s.active[offset]; ok {
@@ -56,8 +56,8 @@ func (s *Seeker) getDecompressedBlock(offset int64, length int) ([]byte, func(),
 		return db.decompressed, db.deref, nil
 	}
 	em.Unlock()
-	buf := s.allocator.Alloc(length)
-	decoded, err := s2.Decode(buf, s.data[offset:][:length])
+	buf := s.allocator.Alloc(uncompressedLength)
+	decoded, err := s2.Decode(buf, s.data[offset:][:compressedLength])
 	if err != nil {
 		return nil, nil, err
 	}

randomaccess.go
@@ -116,7 +116,7 @@ func (s *Seeker) Get(offset, length int64) ([]byte, func(), error) {
 				skipUncompr -= int64(dLen)
 				break
 			}
-			block, deref, err := s.getDecompressedBlock(comprOff+headerSize+checksumSize, dLen)
+			block, deref, err := s.getDecompressedBlock(comprOff+headerSize+checksumSize, len(chunk)-checksumSize, dLen)
 			if err != nil {
 				partialDeref()
 				return nil, nil, err